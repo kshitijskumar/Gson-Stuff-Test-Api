3eface70ede49a662d7c450d723c39736bc4f714
Kshitij Kumar <kshitij.kumar@trell.in>
2021-11-06 23:53:00 +0530

    added application conf file

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,10 +3,17 @@ package com.example
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.application.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
-        configureRouting()
-        configureSerialization()
-    }.start(wait = true)
+//fun main() {
+//    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+//        configureRouting()
+//        configureSerialization()
+//    }.start(wait = true)
+//}
+fun main(args: Array<String>) = EngineMain.main(args = args)
+
+fun Application.module(testing: Boolean = false) {
+    configureRouting()
+    configureSerialization()
 }
